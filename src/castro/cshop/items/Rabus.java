01af2e8f1d0a43a27d21aa072c4cd337eb983dc5
castro12321 <norbertkawinski@gmail.com>
2014-06-11 17:29:25 +0200

    Added /buy rabus

---
+++ src/castro/cshop/items/Rabus.java
@@ -0,0 +1,23 @@
+package castro.cshop.items;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import castro.cshop.items.base.ShopItemData;
+import castro.cshop.items.base.ShopItemSingleUse;
+
+public class Rabus extends ShopItemSingleUse
+{
+	@Override
+    protected int getPricePerUse()
+    {
+	    return 50;
+    }
+	
+	
+    @Override
+    public boolean giveItem(Player player, ShopItemData itemData)
+    {
+		return plugin.broadcast(player.getName() + ChatColor.GREEN + " kupil Rabusa!");
+    }
+}
