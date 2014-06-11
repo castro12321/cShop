326e5c19f84d39508884952679cfbc5760b8f013
castro12321 <norbertkawinski@gmail.com>
2014-06-11 21:28:24 +0200

    Added WorldEdit limit refreshing

--- src/castro/cshop/items/worldedit/WorldEditLimit500k.java
+++ src/castro/cshop/items/worldedit/WorldEditLimit500k.java
@@ -1,5 +1,9 @@
 package castro.cshop.items.worldedit;
 
+import org.bukkit.entity.Player;
+
+import castro.base.Plugin;
+import castro.cshop.items.base.ShopItemData;
 import castro.cshop.items.base.ShopItemPermission;
 
 public class WorldEditLimit500k extends ShopItemPermission
@@ -15,4 +19,21 @@ public class WorldEditLimit500k extends ShopItemPermission
     {
 		return 500;
     }
+	
+	@Override
+	public boolean giveItem(Player player, ShopItemData itemData)
+	{
+		if(!super.giveItem(player, itemData))
+			return false;
+		return Plugin.dispatchCommand(player, "/limit 1");
+	}
+	
+	@Override
+	public boolean takeItem(String playername, ShopItemData itemData)
+	{
+		if(!super.takeItem(playername, itemData))
+			return false;
+		Player player = Plugin.getPlayer(playername);
+		return Plugin.dispatchCommand(player, "/limit 1");
+	}
 }
--- src/castro/cshop/items/worldedit/WorldEditLimitDouble.java
+++ src/castro/cshop/items/worldedit/WorldEditLimitDouble.java
@@ -1,5 +1,9 @@
 package castro.cshop.items.worldedit;
 
+import org.bukkit.entity.Player;
+
+import castro.base.Plugin;
+import castro.cshop.items.base.ShopItemData;
 import castro.cshop.items.base.ShopItemPermission;
 
 public class WorldEditLimitDouble extends ShopItemPermission
@@ -16,4 +20,21 @@ public class WorldEditLimitDouble extends ShopItemPermission
 		final float perDay = 100;
 		return perDay/24;
     }
+	
+	@Override
+	public boolean giveItem(Player player, ShopItemData itemData)
+	{
+		if(!super.giveItem(player, itemData))
+			return false;
+		return Plugin.dispatchCommand(player, "/limit 1");
+	}
+	
+	@Override
+	public boolean takeItem(String playername, ShopItemData itemData)
+	{
+		if(!super.takeItem(playername, itemData))
+			return false;
+		Player player = Plugin.getPlayer(playername);
+		return Plugin.dispatchCommand(player, "/limit 1");
+	}
 }
